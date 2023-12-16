e6841e750bacec0316fc62700b4b64c0e5ea2957
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-12-17 03:24:15 +0900

    feature/add-list-tasks: タスクリストの取得機能を追加

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,11 +1,15 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
 
-@router.get("/tasks")
+@router.get("/tasks", response_model=List[task_schema.Task])
 async def list_tasks():
-    pass
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
 @router.post("/tasks")
